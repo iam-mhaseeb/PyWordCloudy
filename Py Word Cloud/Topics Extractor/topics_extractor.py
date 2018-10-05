2067713b0d33486f365aa4487c0be74a0d935a43
Muhammad Haseeb <muhammad.haseeb@arbisoft.com>
2018-10-05 16:14:17 +0500

    Tokenization Implemented

--- Py Word Cloud/Topics Extractor/topics_extractor.py
+++ Py Word Cloud/Topics Extractor/topics_extractor.py
@@ -4,12 +4,30 @@ import os
 class TopicsExtractor:
 
     """This class is responsible for exctracting concepts
-    from text file read by main module and return dictionary
-    of topics extracted from file."""
+        from text string sent by main module and return
+        dictionary of topics extracted from string."""
 
-    def __init__(self, text_input_file):
+    def __init__(self, input_txt):
 
         """This is constructor for topics extractor class
-        it takes text input file and expose it to whole class."""
+            it takes text string and act as controller to
+            expose text string to whole class."""
 
-        self.text_input_file = text_input_file
+        input_txt = input_txt
+        tokenized_txt = self.get_tokenized_text(self.input_text)
+
+    def get_tokenized_text(self, input_text):
+
+        """This function does tokenization of text string
+            and return tokenized words as list.
+
+        Parameters
+        ----------
+        text string : iterable of characters(string).
+        Returns
+        -------
+        tokenized text : list of strings each string
+            representing a word.
+        """
+
+        return input_txt.split()

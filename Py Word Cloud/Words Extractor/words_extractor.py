97cecb5ef99cd66c01674f17c459b025a146ea57
Muhammad Haseeb <muhammad.haseeb@arbisoft.com>
2018-10-05 17:29:59 +0500

    Words Extractor Class Implemented

--- Py Word Cloud/Words Extractor/words_extractor.py
+++ Py Word Cloud/Words Extractor/words_extractor.py
@@ -1,4 +1,4 @@
-import os
+import nltk
 
 
 class WordsExtractor:
@@ -14,9 +14,9 @@ class WordsExtractor:
             it takes text string and act as controller to
             expose text string to whole class."""
 
-        self.extracted_words = None
         tokenized_words = self.get_tokenized_text(input_txt)
         cleaned_words = self.remove_stop_words(tokenized_words)
+        self.extracted_words = self.tag_words(cleaned_words)
 
     def get_tokenized_text(self, input_txt):
 
@@ -41,13 +41,32 @@ class WordsExtractor:
 
         Parameters
         ----------
-        tokenized_txt : iterable of tokenized string.
+        tokenized_txt : iterable of tokenized strings.
         Returns
         -------
         cleaned text : list of cleaned non stop words strings
              each string representing a word.
         """
-        file_path = r"Py Word Cloud/Topics Extractor/stop_words_list.txt"
+
+        file_path = r"Py Word Cloud/Words Extractor/stop_words_list.txt"
         with open(file_path, "r") as stop_words_file:
             stop_words = stop_words_file.read()
             return set([word for word in tokenized_txt if word not in stop_words])
+
+    def tag_words(self, cleaned_words):
+
+        """This function tags words from cleaned words list
+            and return only nouns, verbs and pronoun as list.
+
+        Parameters
+        ----------
+        cleaned_words : iterable of cleaned strings.
+        Returns
+        -------
+        tegged_words : list of tagged words strings each string
+            representing a noun, verb or pronoun.
+        """
+
+        tagged_words = nltk.pos_tag(cleaned_words)
+        tags = ["NN", "NNS", "NNP", "NNPS", "VB", "VBD", "VBG", "VBN", "VBP", "VBZ"]
+        return [word for word, tag in tagged_words if tag in tags]

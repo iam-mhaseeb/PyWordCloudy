6d455710e09abb1e9e4064dae94a598f243e067e
Muhammad Haseeb <muhammad.haseeb@arbisoft.com>
2018-10-05 16:25:47 +0500

    Removing Stop Words Implemented

--- Py Word Cloud/Topics Extractor/topics_extractor.py
+++ Py Word Cloud/Topics Extractor/topics_extractor.py
@@ -14,16 +14,16 @@ class TopicsExtractor:
             expose text string to whole class."""
 
         input_txt = input_txt
-        tokenized_txt = self.get_tokenized_text(self.input_text)
+        tokenized_txt = self.get_tokenized_text(self.input_txt)
 
-    def get_tokenized_text(self, input_text):
+    def get_tokenized_text(self, input_txt):
 
         """This function does tokenization of text string
             and return tokenized words as list.
 
         Parameters
         ----------
-        text string : iterable of characters(string).
+        input_txt : iterable of characters(string).
         Returns
         -------
         tokenized text : list of strings each string
@@ -31,3 +31,21 @@ class TopicsExtractor:
         """
 
         return input_txt.split()
+
+    def remove_stop_words(self, tokenized_txt):
+
+        """This function removes stop words from tokenized
+            words list and return remaining words as list.
+
+        Parameters
+        ----------
+        tokenized_txt : iterable of tokenized string.
+        Returns
+        -------
+        cleaned text : list of cleaned non stop words strings
+             each string representing a word.
+        """
+
+        with open(("stop_words_list.txt"), "r") as stop_words_file:
+            stop_words = stop_words_file.read()
+            return [word for word in tokenized_txt if word not in stop_words]

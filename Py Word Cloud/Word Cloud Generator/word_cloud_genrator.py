40b6aef5ca799321a9fd80550592edcd49d1fc05
Muhammad Haseeb <muhammad.haseeb@arbisoft.com>
2018-10-08 18:00:23 +0500

    Word Cloud Genrator Introduced

---
+++ Py Word Cloud/Word Cloud Generator/word_cloud_genrator.py
@@ -0,0 +1,8 @@
+from PIL import Image, ImageDraw
+
+
+class WordCloudGenrator:
+
+    """This class is responsible for genrating image & writing
+        words from text string sent by main module on image and
+        store it on disk."""

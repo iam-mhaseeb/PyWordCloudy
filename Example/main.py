2007b3f50d79a1e96c02bfedf184242331e926b3
Muhammad Haseeb <muhammad.haseeb@arbisoft.com>
2018-10-16 17:48:00 +0500

    Droping Words That Can't Fit Implemented

--- Example/main.py
+++ Example/main.py
@@ -1,9 +1,51 @@
 from pywordcloud import WordCloudGenrator
 
-test_txt = """A computer is a programmable machine. The two principal
-                characteristics of a computer are: It responds to a
-                specific set of instructions in a well-defined manner
-                and it can execute a prerecorded list of instructions
-                (a program)."""
+test_txt = """Every enterprise needs to innovate. It doesn’t matter whether you’re are a profit-seeking business, a nonprofit organization, or a government entity. The simple truth is that every business model fails eventually, because conditions change over time. We have to manage not for stability, but for disruption — that, or face irrelevance.
+
+There is no shortage of advice on how to go about it. In fact, there is far too much advice. Design thinkers will tell you to focus on the end user, but Harvard’s Clayton Christensen says that listening to customers too much is how good businesses fail. Then there’s open innovation, lean startups, and on and on it goes.
+
+The truth is that there is no one path to innovation. Everybody has to find their own way. Just because someone had success with one strategy doesn’t mean it’s right for the problem you need to solve. So the best advice is to gather as many tools for your toolbox as you can.
+
+Here are four facts about innovation that you’ll rarely hear, though they’re critically important.
+
+1. Your Success Often Works Against You
+For the most part, managers aren’t responsible for innovation. As the title implies, they’re responsible for managing operations. That involves hiring and empowering strong employees, optimizing practices and processes, and reducing errors and mistakes. Managers aren’t generally trying to build a better mousetrap; they’re trying to run things smoothly and efficiently.
+
+It’s easy for someone to stand up on stage at a conference and paint operational managers as dimwits with their heads in the sand. But managing a quality operation is a tough job that requires talent, dedication, and skill. So unless you’ve actually done the job, don’t be too quick to judge.
+
+However, managers do need to realize that there is a fundamental trade-off between innovating and optimizing operations. Running efficient operations requires standardization and control to yield predictable outcomes. Innovation, on the other hand requires experimentation. It means trying a lot of new things, most of which are going to fail.
+
+That’s why success so often leads to failure. What makes you successful in one competitive environment will likely be a hindrance when things change. So you need to find a healthy balance between squeezing everything you can out of the present, while still leaving room to create and build for the future.
+
+2. Look For A Hair-On-Fire Use Case
+Good operational managers learn to identify large addressable markets. Bigger markets help you scale your business, drive revenues, and allow you invest back into operations to create more efficiency. Greater efficiencies lead to fatter profit margins, which allow you to invest even more in improvements, creating a virtuous cycle.
+
+Yet, when you’re trying to do something truly new and different, trying to scale too fast can kill your business before it’s even gotten started. A truly revolutionary product is unpredictable because, by its very nature, it’s not well understood. Charging boldly into the unknown is a sure way to run into unanticipated problems that are expensive to fix at scale.
+
+Innovation is never a single event. It is a long process of discovery, engineering, and transformation.
+A better strategy is to identify a hair-on-fire use case — someone who needs a problem fixed so badly that they are willing to overlook the inevitable glitches. They will help you identify shortcomings early and correct them. Once you get things ironed out, you can begin to scale for more ordinary use cases.
+
+For example, developing a self-driving car is a risky proposition with a dizzying amount of variables you can’t account for. However, a remote mine in Western Australia, where drivers are scarce and traffic nonexistent, is an ideal place to test and improve the technology. In a similar vein, Google Glass failed utterly as a mass product, but is getting a second life as an industrial tool. Sometimes it’s better to build for the few than the many.
+
+3. Start With the Monkey
+When I work with executives, they often have a breakthrough idea that excites them. They begin to tell me what a great opportunity it is and how they are perfectly positioned to capitalize on it. However, when I dig deeper, there’s always a major barrier to making it happen. When I ask about that, they shut down.
+
+Make no mistake. Innovation isn’t about ideas, it’s about solving problems. The truth is that nobody cares about your ideas, they care about the problems you can solve for them. The reason most people can’t innovate isn’t because they don’t have ideas, but because they lack the perseverance needed to stick with a really tough problem until it’s cracked.
+
+At Google X, the tech giant’s “moonshot factory,” the mantra is #MonkeyFirst. The idea is that if you want to get a monkey to recite Shakespeare on a pedestal, you start by training the monkey, not building the pedestal. Because training the monkey is the hard part. Anyone can build a pedestal.
+
+The problem is that most people start with the pedestal, because it’s what they know. And by building it, they can show early progress against a timeline. Unfortunately, building a pedestal gets you nowhere. Unless you can actually train the monkey, working on the pedestal is wasted effort.
+
+4. Sometimes the World Isn’t Ready Yet
+When Alexander Fleming first published his discovery of penicillin, no one really noticed. When Xerox executives first got a look at the Alto — the machine that would become the model for the Macintosh seven years later — they didn’t see what the big deal was. When Jim Allison first showed pharmaceutical executives his idea for cancer immunotherapy, not one would invest in it.
+
+We always think the next big thing will be obvious, but in truth, it often starts out looking like nothing at all. When something truly has the power to change the world, the world isn’t ready for it. It needs to build advocacy, gain traction among a particular industry or field, and combine with other innovations before it can make an impact.
+
+But no one ever tells you that. We’re conditioned to think that someone like Steve Jobs or Elon Musk stands up on stage, announces that the world has changed, and everybody just goes along. It never really happens that way because innovation is never a single event. It is a long process of discovery, engineering, and transformation that usually takes about 30 years.
+
+“Don’t worry about people stealing your ideas,” said the computing pioneer Howard Aiken. “If your ideas are any good, you’ll have to ram them down people’s throats.” Never were truer words spoken. Great innovators aren’t just people with ideas. They’re people willing to stick it out and take the shots from people who ridicule them. Eventually, if they’re lucky, they really do change the world.
+
+"""
 
 word_cloud = WordCloudGenrator(input_txt=test_txt)
+
--- pywordcloud/word_cloud_genrator.py
+++ pywordcloud/word_cloud_genrator.py
@@ -6,8 +6,9 @@ import inspect
 import operator
 from itertools import groupby
 from PIL import Image, ImageDraw, ImageFont
-from words_extractor import WordsExtractor  # TODO: Make it .words_extractor
+from pywordcloud.words_extractor import  WordsExtractor
 
+random.seed()
 FONT_PATH = os.path.join(os.path.dirname(__file__), "font.ttf")
 
 
@@ -67,40 +68,48 @@ class WordCloudGenrator:
         sorted_words = sorted(
             words_freq.items(), key=operator.itemgetter(1), reverse=True)
         cloud_styles = {}
-        font_size = 30
+        font_size = 24
         max_freq = sorted_words[0][1]
         dimens = []
 
         for word_freq in sorted_words:
-            if word_freq[1] < max_freq and font_size > 12:
+            if word_freq[1] < max_freq and font_size > 8:
                 max_freq = word_freq[1]
                 font_size = int(font_size/1.5)
             fnt = ImageFont.truetype(FONT_PATH, font_size)
-            # TODO: Write text on new img paste new image on word cloud
+            size = fnt.getsize(word_freq[0])
             dimens_set = False
+            drop_counter = 0
             while not dimens_set:
-                width = random.randint(50, 400)
-                height = random.randint(10, 350)
+
+                if drop_counter >= 50:  # Try 50 times Drop the word the if not fitting
+                    break
+
+                x = random.randint(20, 450)
+                y = random.randint(10, 350)
 
                 for dimen in dimens:
-                    if dimen[0]-5 <= width <= dimen[0]+10:
+                    if dimen[0] - 5 <= x <= dimen[0] + size[0]:
+                        drop_counter += 1
                         break
-                    if dimen[1]-30 <= height <= dimen[1]+30:
+                    if dimen[1] - 20 <= y <= dimen[1] + size[1]:
+                        drop_counter += 1
                         break
 
                 else:
                     dimens_set = True
 
-            dimens.append([width, height])
-            r = random.randint(1, 200)
-            g = random.randint(1, 200)
-            b = random.randint(1, 200)
-            cloud_styles[word_freq[0]] = {
-                "font": fnt,
-                "width": width,
-                "height": height,
-                "r": r, "g": g, "b": b
-                }
+            if drop_counter == 0:
+                dimens.append([x, y])
+                r = random.randint(1, 200)
+                g = random.randint(1, 200)
+                b = random.randint(1, 200)
+                cloud_styles[word_freq[0]] = {
+                    "font": fnt,
+                    "width": x,
+                    "height": y,
+                    "r": r, "g": g, "b": b
+                    }
 
         return cloud_styles
 
@@ -156,16 +165,3 @@ class WordCloudGenrator:
             font weight representing the frequency of word."""
 
         return self.word_cloud
-
-
-test_txt = """Pakistan[b] (Urdu: پاکِستان‬‎), officially the Islamic Republic of Pakistan (Urdu: اِسلامی جمہوریہ پاکِستان‬‎), is a country in South Asia. It is the fifth-most populous country with a population exceeding 212,742,631 people.[19] In area, it is the 33rd-largest country, spanning 881,913 square kilometres (340,509 square miles). Pakistan has a 1,046-kilometre (650-mile) coastline along the Arabian Sea and Gulf of Oman in the south and is bordered by India to the east, Afghanistan to the west, Iran to the southwest, and China in the far northeast. It is separated narrowly from Tajikistan by Afghanistan's Wakhan Corridor in the northwest, and also shares a maritime border with Oman.
-
-The territory that now constitutes Pakistan was the site of several ancient cultures, including the Mehrgarh of the Neolithic and the Bronze Age Indus Valley Civilisation, and was later home to kingdoms ruled by people of different faiths and cultures, including Hindus, Indo-Greeks, Muslims, Turco-Mongols, Afghans, and Sikhs. The area has been ruled by numerous empires and dynasties, including the Persian Achaemenid Empire, Alexander III of Macedon, the Seleucid Empire, the Indian Maurya Empire, the Gupta Empire,[26] the Arab Umayyad Caliphate, the Delhi Sultanate, the Mongol Empire, the Mughal Empire, the Afghan Durrani Empire, the Sikh Empire (partially), and, most recently, the British Empire.
-
-Pakistan is the only country to have been created in the name of Islam.[27][28] As a result of the Pakistan Movement led by Muhammad Ali Jinnah and the Indian subcontinent's struggle for independence, the sovereign state of Pakistan was created in 1947 as an independent homeland for Indian Muslims.[29] It is an ethnically and linguistically diverse country, with a similarly diverse geography and wildlife. Initially a dominion, Pakistan adopted a constitution in 1956, becoming an Islamic republic. An ethnic civil war in 1971 resulted in the secession of East Pakistan as the new country of Bangladesh.[30] In 1973 Pakistan adopted a new constitution establishing, alongside its pre-existing parliamentary republic status, a federal government based in Islamabad consisting of four provinces and three federal territories. The new constitution also stipulated that all laws are to conform to the injunctions of Islam as laid down in the Quran and Sunnah.[31]
-
-A regional[32][33][34] and middle power,[35][36][37] Pakistan has the sixth-largest standing armed forces in the world and is also a nuclear power as well as a declared nuclear-weapons state, the second in South Asia and the only nation in the Muslim world to have that status. Pakistan has a semi-industrialised economy with a well-integrated agriculture sector and a growing services sector.[38][39] The Pakistani economy is the 24th-largest in the world in terms of purchasing power and the 41st-largest in terms of nominal GDP (World Bank). It is ranked among the emerging and growth-leading economies of the world,[40][41] and is backed by one of the world's largest and fastest-growing middle class.[42][43]
-
-Pakistan's political history since independence has been characterized by periods of military rule, political instability and conflicts with India. The country continues to face challenging problems, including overpopulation, terrorism, poverty, illiteracy, and corruption.[44][45][46][47] Pakistan is a member of the United Nations, the Shanghai Cooperation Organisation, the Non-Aligned Movement, the Organisation of Islamic Cooperation, the Commonwealth of Nations, the Economic Cooperation Organisation, the South Asian Association for Regional Cooperation, the Developing Eight, and the G20 developing nations, Group of 24, Group of 77, and ECOSOC. It is also an associate member of CERN. Pakistan is a signatory to the Kyoto Protocol, the Paris Agreement, and the International Covenant on Civil and Political Rights."""
-
-word_cloud = WordCloudGenrator(input_txt=test_txt)
